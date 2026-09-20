4c56ee46d6bafc520f750faf91988bacc9f626ee
Michalsen82 <michal.bukowski@pereko.pl>
2026-09-20 23:02:55 +0200

    Ustaw wersję aplikacji 2.0.4

--- pwa.js
+++ pwa.js
@@ -6,7 +6,7 @@
     notifyDismissed:'pereko_pwa_notify_dismissed'
   };
   const DEFAULT_PREFS={assignment:true,taskDone:true,comments:true,deadline:true,files:true};
-  const APP_VERSION='2.0.3';
+  const APP_VERSION='2.0.4';
   const VERSION_URL='/app-version.json';
   let latestPublishedVersion=null;
   let updateBanner=null;
